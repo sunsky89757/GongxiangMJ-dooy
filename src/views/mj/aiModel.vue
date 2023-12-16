e897c8513277b5a05a396caa4ddfc6752b7c63a7
sunsky89757 <113254524+sunsky89757@users.noreply.github.com>
2023-12-16 16:26:11 +0800

    Update aiModel.vue

--- src/views/mj/aiModel.vue
+++ src/views/mj/aiModel.vue
@@ -5,10 +5,8 @@ import {gptConfigStore} from '@/store'
 
 const emit = defineEmits(['close']);
 const config = ref({
-model:[ 'gpt-4','gpt-3.5-turbo',`gpt-4-1106-preview`,`gpt-3.5-turbo-16k`,'gpt-4-0314','gpt-4-0613','gpt-4-32k-0613' ,'gpt-4-32k','gpt-4-32k-0314',`gpt-3.5-turbo-16k-0613`
-,`gpt-4-vision-preview`,`gpt-3.5-turbo-1106` 
-,'gpt-3.5-turbo-0301','gpt-3.5-turbo-0613','gpt-4-all','gpt-3.5-net']
-,maxToken:2048
+model:[ 'gpt-4','gpt-3.5-turbo-1106','gpt-4-1106-preview','gpt-4-32k','gpt-4-vision-preview','gpt-4-all','dall-e-3','gemini-pro','gemini-pro-vision','google-palm','google-palm']
+,maxToken:128000
 }); 
 const modellist = computed(() => { //
     let rz =[ ];
