0e634276c50463e1f738659e1f08764caa99f075
sunsky89757 <sunsky20120101@163.com>
2024-01-26 16:04:39 +0800

    新增模型

--- src/views/mj/aiModel.vue
+++ src/views/mj/aiModel.vue
@@ -17,6 +17,8 @@ const config = ref({
 model:[ 
 			'gpt-3.5-turbo',
 			'gpt-3.5-turbo-1106',
+            'gpt-4-turbo-preview',
+            'gpt-4-0125-preview',
 			'gpt-4-1106-preview',
 			'gpt-4',
 			'gpt-4-32k',
